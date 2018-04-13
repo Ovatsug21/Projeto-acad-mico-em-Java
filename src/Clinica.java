9675bdb5ddb49f3200b05a6008f09fa4caf49244
Usuario <Usuario@LittleFoot>
2018-04-12 22:56:30 -0300

    primeiro comitt

---
+++ src/Clinica.java
@@ -0,0 +1,34 @@
+
+public class Clinica {
+    
+    private int senha;
+    private String nomeProfissional;
+    
+    public Clinica(int senha,String nomeProfissional) {
+    
+        this.senha = senha;
+        this.nomeProfissional = nomeProfissional;
+    }    
+    public Clinica (){
+    
+    }    
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public String getNomeProfissional() {
+        return nomeProfissional;
+    }
+
+    public void setNomeProfissional(String nomeProfissional) {
+        this.nomeProfissional = nomeProfissional;
+    }
+
+
+   
+}
